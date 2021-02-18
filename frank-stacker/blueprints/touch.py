3256401c5fee64da3c79034dd629c585c487a0bc
eddlabi <eddlabi@gmail.com>
2021-02-18 14:42:53 +0100

    initial commit

---
+++ frank-stacker/blueprints/touch.py
@@ -0,0 +1,237 @@
+import copy
+
+from troposphere import (
+    Ref, FindInMap, Not, Equals, And, Condition, Join, ec2, autoscaling,
+    If, GetAtt, Output
+)
+from troposphere import elasticloadbalancing as elb
+from troposphere.autoscaling import Tag as ASTag
+from troposphere.route53 import RecordSetType
+
+from stacker.blueprints.base import Blueprint
+from stacker.blueprints.variables.types import TroposphereType
+from stacker.blueprints.variables.types import (
+    CFNCommaDelimitedList,
+    CFNNumber,
+    CFNString,
+    EC2VPCId,
+    EC2KeyPairKeyName,
+    EC2SecurityGroupId,
+    EC2SubnetIdList,
+)
+
+CLUSTER_SG_NAME = "%sSG"
+ELB_SG_NAME = "%sElbSG"
+ELB_NAME = "%sDev-ELB"
+
+
+class AutoscalingGroup(Blueprint):
+    VARIABLES = {
+        'VpcId': {'type': EC2VPCId, 'description': 'Vpc Id', 'default': "vpc-test"},
+        'DefaultSG': {'type': EC2SecurityGroupId,
+                      'description': 'Top level security group.',
+                      'default': 'sg-3245'},
+        'BaseDomain': {
+            'type': CFNString,
+            'default': '',
+            'description': 'Base domain for the stack.'},
+        'PrivateSubnets': {'type': EC2SubnetIdList,
+                           'description': 'Subnets to deploy private '
+                                          'instances in.',
+                           'default': ['10.100.1.0/24', '10.100.2.0/24']},
+        'PublicSubnets': {'type': EC2SubnetIdList,
+                          'description': 'Subnets to deploy public (elb) '
+                                         'instances in.',
+                          'default': ['10.200.1.0/24', '10.200.2.0/24']},
+        'AvailabilityZones': {'type': CFNCommaDelimitedList,
+                              'description': 'Availability Zones to deploy '
+                                             'instances in.',
+                              'default': ['']},
+        'InstanceType': {'type': CFNString,
+                         'description': 'EC2 Instance Type',
+                         'default': 't2.micro'},
+        'MinSize': {'type': CFNNumber,
+                    'description': 'Minimum # of instances.',
+                    'default': '1'},
+        'MaxSize': {'type': CFNNumber,
+                    'description': 'Maximum # of instances.',
+                    'default': '5'},
+        'SshKeyName': {'type': EC2KeyPairKeyName},
+        'ImageName': {
+            'type': CFNString,
+            'description': 'The image name to use from the AMIMap (usually '
+                           'found in the config file.)',},
+        'ELBHostName': {
+            'type': CFNString,
+            'description': 'A hostname to give to the ELB. If not given '
+                           'no ELB will be created.',
+            'default': ''},
+        'ELBCertName': {
+            'type': CFNString,
+            'description': 'The SSL certificate name to use on the ELB.',
+            'default': ''},
+        'ELBCertType': {
+            'type': CFNString,
+            'description': 'The SSL certificate type to use on the ELB.',
+            'default': ''},
+    }
+
+    def create_conditions(self):
+        self.template.add_condition(
+            "CreateELB",
+            Not(Equals(Ref("ELBHostName"), "")))
+        self.template.add_condition(
+            "SetupDNS",
+            Not(Equals(Ref("BaseDomain"), "")))
+        self.template.add_condition(
+            "UseSSL",
+            Not(Equals(Ref("ELBCertName"), "")))
+        self.template.add_condition(
+            "CreateSSLELB",
+            And(Condition("CreateELB"), Condition("UseSSL")))
+        self.template.add_condition(
+            "SetupELBDNS",
+            And(Condition("CreateELB"), Condition("SetupDNS")))
+        self.template.add_condition(
+            "UseIAMCert",
+            Not(Equals(Ref("ELBCertType"), "acm")))
+
+    def create_security_groups(self):
+        t = self.template
+        asg_sg = CLUSTER_SG_NAME % self.name
+        elb_sg = ELB_SG_NAME % self.name
+        t.add_resource(ec2.SecurityGroup(
+            asg_sg,
+            GroupDescription=asg_sg,
+            VpcId=Ref("VpcId")))
+        # ELB Security group, if ELB is used
+        t.add_resource(
+            ec2.SecurityGroup(
+                elb_sg,
+                GroupDescription=elb_sg,
+                VpcId=Ref("VpcId"),
+                Condition="CreateELB"))
+        # Add SG rules here
+        # Allow ELB to connect to ASG on port 80
+        t.add_resource(ec2.SecurityGroupIngress(
+            "%sElbToASGPort80" % self.name,
+            IpProtocol="tcp", FromPort="80", ToPort="80",
+            SourceSecurityGroupId=Ref(elb_sg),
+            GroupId=Ref(asg_sg),
+            Condition="CreateELB"))
+        # Allow Internet to connect to ELB on port 80
+        t.add_resource(ec2.SecurityGroupIngress(
+            "InternetTo%sElbPort80" % self.name,
+            IpProtocol="tcp", FromPort="80", ToPort="80",
+            CidrIp="0.0.0.0/0",
+            GroupId=Ref(elb_sg),
+            Condition="CreateELB"))
+        t.add_resource(ec2.SecurityGroupIngress(
+            "InternetTo%sElbPort443" % self.name,
+            IpProtocol="tcp", FromPort="443", ToPort="443",
+            CidrIp="0.0.0.0/0",
+            GroupId=Ref(elb_sg),
+            Condition="CreateSSLELB"))
+
+    def setup_listeners(self):
+        no_ssl = [elb.Listener(
+            LoadBalancerPort=80,
+            Protocol='HTTP',
+            InstancePort=80,
+            InstanceProtocol='HTTP'
+        )]
+
+        # Choose proper certificate source
+        acm_cert = Join("", [
+            "arn:aws:acm:", Ref("AWS::Region"), ":", Ref("AWS::AccountId"),
+            ":certificate/", Ref("ELBCertName")])
+        iam_cert = Join("", [
+            "arn:aws:iam::", Ref("AWS::AccountId"), ":server-certificate/",
+            Ref("ELBCertName")])
+        cert_id = If("UseIAMCert", iam_cert, acm_cert)
+
+        with_ssl = copy.deepcopy(no_ssl)
+        with_ssl.append(elb.Listener(
+            LoadBalancerPort=443,
+            InstancePort=80,
+            Protocol='HTTPS',
+            InstanceProtocol="HTTP",
+            SSLCertificateId=cert_id))
+        listeners = If("UseSSL", with_ssl, no_ssl)
+
+        return listeners
+
+    def create_load_balancer(self):
+        t = self.template
+        elb_name = ELB_NAME % self.name
+        elb_sg = ELB_SG_NAME % self.name
+        t.add_resource(elb.LoadBalancer(
+            elb_name,
+            HealthCheck=elb.HealthCheck(
+                Target='HTTP:80/',
+                HealthyThreshold=3,
+                UnhealthyThreshold=3,
+                Interval=5,
+                Timeout=3),
+            Listeners=self.setup_listeners(),
+            SecurityGroups=[Ref(elb_sg), ],
+            Subnets=Ref("PublicSubnets"),
+            Condition="CreateELB"))
+
+        # Setup ELB DNS
+        t.add_resource(
+            RecordSetType(
+                '%sDnsRecord' % elb_name,
+                # Appends a '.' to the end of the domain
+                HostedZoneName=Join("", [Ref("BaseDomain"), "."]),
+                Comment='Router ELB DNS',
+                Name=Join('.', [Ref("ELBHostName"), Ref("BaseDomain")]),
+                Type='CNAME',
+                TTL='120',
+                ResourceRecords=[
+                    GetAtt(elb_name, 'DNSName')],
+                Condition="SetupELBDNS"))
+
+    def get_launch_configuration_parameters(self):
+        return {
+            'ImageId': FindInMap('AmiMap', Ref("AWS::Region"),
+                                 Ref('ImageName')),
+            'InstanceType': Ref("InstanceType"),
+            'KeyName': Ref("SshKeyName"),
+            'SecurityGroups': self.get_launch_configuration_security_groups(),
+        }
+
+    def get_autoscaling_group_parameters(self, launch_config_name, elb_name):
+        return {
+            'AvailabilityZones': Ref("AvailabilityZones"),
+            'LaunchConfigurationName': Ref(launch_config_name),
+            'MinSize': Ref("MinSize"),
+            'MaxSize': Ref("MaxSize"),
+            'VPCZoneIdentifier': Ref("PrivateSubnets"),
+            'LoadBalancerNames': If("CreateELB", [Ref(elb_name), ], []),
+            'Tags': [ASTag('Name', self.name, True)],
+        }
+
+    def get_launch_configuration_security_groups(self):
+        sg_name = CLUSTER_SG_NAME % self.name
+        return [Ref("DefaultSG"), Ref(sg_name)]
+
+    def create_autoscaling_group(self):
+        name = "%sASG" % self.name
+        launch_config = "%sLaunchConfig" % name
+        elb_name = ELB_NAME % self.name
+        t = self.template
+        t.add_resource(autoscaling.LaunchConfiguration(
+            launch_config,
+            **self.get_launch_configuration_parameters()
+        ))
+        t.add_resource(autoscaling.AutoScalingGroup(
+            name,
+            **self.get_autoscaling_group_parameters(launch_config, elb_name)
+        ))
+
+    def create_template(self):
+        self.create_conditions()
+        self.create_security_groups()
+        self.create_load_balancer()
+        self.create_autoscaling_group()
---
+++ frank-stacker/readme.rst
@@ -0,0 +1,85 @@
+first_stacker_project
+####################
+
+A `Cookiecutter <https://github.com/audreyr/cookiecutter>`_ (project template)
+for creating a barebone
+`stacker <https://github.com/cloudtools/stacker#stacker>`_ project
+
+Note:
+ You do not have to setup your project this way, this is just meant as a
+ suggestion and some simple guidance to help folks who are new to stacker.
+
+Requirements
+============
+
+* Python 2.7 or 3.4+
+* `cookiecutter <https://cookiecutter.readthedocs.io/en/latest/installation.html>`_
+* `poetry <https://poetry.eustace.io/>`_
+
+Usage
+=====
+
+1. Generate a ``stacker`` project, following the prompts from the command.
+
+      .. code-block:: bash
+
+          $ cookiecutter gh:cloudtools/stacker_cookiecutter
+          project_name [myproject]:
+          stacker_bucket [stacker-myproject]:
+          repo_name [myproject]:
+          description [stacker project for myproject]:
+
+This command will create a new stacker project in your present working
+directory. 
+
+Note: You should try to make your ``stacker_bucket`` variable unique
+since S3 bucket names share a global namespace. It will default to
+``stacker-first_stacker_project`` but you may choose any unique value.
+
+Project tree
+=================
+
+In this example we have a product called ``frank-stacker`` and two environments
+called ``dev`` and ``prod``.
+
+Some notes about the files in this tree:
+
+**conf/<env>.env**:
+ This is an "environment" file which holds variables that change in the config
+ based on the environment. This allows you to have a single config for all
+ your environments, while changing small things per environment.
+
+ See: http://stacker.readthedocs.io/en/latest/environments.html
+
+**stacker.yaml**:
+ This is a "stacker config" file.
+
+ See: http://stacker.readthedocs.io/en/latest/config.html
+
+**blueprints/touch.py**:
+ This is a tiny ``stacker blueprint`` that doesn't do much of anything.
+ A blueprint is used to programatically generate CloudFormation JSON.
+
+ See: http://stacker.readthedocs.io/en/latest/blueprints.html
+
+**tests/blueprints/test_touch.py**:
+  This is a tiny ``stacker blueprint`` test which only creates a simple
+  resource in CloudFormation (a WaitCondition, which does nothing on it's own).
+
+  See: http://stacker.readthedocs.io/en/latest/blueprints.html#testing-blueprints
+
+Running a release
+====================
+
+In this example we use a ``Makefile`` to save commands.  The commands will be
+ran using **poetry** which will handle creating a virtualenv for you, as well
+as insuring that the correct packages are installed.
+
+To execute stacker using your dev environment, using the *--interactive* flag
+run::
+
+ make dev ARGS=--interactive
+
+To execute stacker using the prod environment, run::
+
+ make prod ARGS=--interactive
